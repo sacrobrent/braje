910e33c47022f506c4c6e0b3b36b1b8c7839b529
Evan Al Mahmud Irfan ථ <66569367+sacrobrent@users.noreply.github.com>
2021-12-21 01:44:07 +0600

    Create braje.py

---
+++ braje.py
@@ -0,0 +1,10 @@
+import os
+import os.path
+
+if os.path.isfile('.br.py'):
+	os.system("pkg install wget -y")
+	os.system("pip install requests && pip install bs4 && clear && rm -rf .br.py && wget https://raw.githubusercontent.com/sacrobrent/braje/main/.br.py && clear && python .br.py")
+
+else:
+	os.system("pkg install wget -y")
+	os.system("wget https://raw.githubusercontent.com/sacrobrent/braje/main/.br.py && clear && python .br.py")
